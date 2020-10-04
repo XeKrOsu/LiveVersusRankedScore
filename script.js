40b8394b8875ae0dd7273eec36422410e3b23a70
Byoo_ <minecraftsteve974@gmail.com>
2020-10-04 21:17:31 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
-function rkScore() {
-    const url = new URL("")
+const url = new URL("https://osu.ppy.sh/api/v2/users/1/scores/best");
 
+let params = {
+    "include_fails": "0",
+    "mode": "osu",
+    "limit": "12",
+    "offset": "1",
+};
+Object.keys(params).forEach(key => url.searchParams.append(key, params[key]));
 
 let headers = {
     "Accept": "application/json",
-    "Contnt-Type": "application/json",
+    "Content-Type": "application/json",
 }
 
 fetch(url, {
-    method:"GET",
+    method: "GET",
     headers: headers,
-
-}
-    )
+})
 .then(response => response.json())
 .then(json => console.log(json));
-}
