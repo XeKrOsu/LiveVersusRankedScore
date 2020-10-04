a7116a653dd0132c460e6331e92bd9bf14c5a872
Byoo_ <minecraftsteve974@gmail.com>
2020-10-04 22:02:40 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,28 @@
-fetch("https://osu.ppy.sh/api/v2/users/1/osu", {
-    headers: {
-      Authorization: 'Bearer BWzJJTNhoDEgACWDvIH8897nGnAkBBflUkG3vVGE'
-    }
-});
+//fetch("https://osu.ppy.sh/api/v2/users/1/osu", {
+//    headers: {
+//      Authorization: 'Bearer BWzJJTNhoDEgACWDvIH8897nGnAkBBflUkG3vVGE'
+//    }
+//});
+//
+//const url = new URL("https://osu.ppy.sh/api/v2/users/1/osu");
+//
+//let headers = {
+//    "Accept": "application/json",
+//    "Content-Type": "application/json",
+//}
+//
+//fetch(url, {
+//    method: "GET",
+//    headers: headers
+//})
+//.then(response => response.json())
+//.then(json => console.log(json));
 
-const url = new URL("https://osu.ppy.sh/api/v2/users/1/osu");
+const baseURL = "https://osu.ppy.sh/api/get_user?k=4cf18ced7dbee5e604b829e96ba2118a9e3112cd";
 
-let headers = {
-    "Accept": "application/json",
-    "Content-Type": "application/json",
-}
+let username = "Soulphase";
+let mode = 0;
 
-fetch(url, {
-    method: "GET",
-    headers: headers,
-})
-.then(response => response.json())
-.then(json => console.log(json));
+let user = JSON.parse(fetch(baseURL + "&u=" + username + "&m=" + mode));
+
+console.log(user);
